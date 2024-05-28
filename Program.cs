46dd2207323eaf55fe2abd4a79788ac4d5256de9
Olamide Onakoya <123734265+koya-deimos@users.noreply.github.com>
2024-05-28 17:22:57 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,8 +19,8 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-
-app.MapGet("/", () =>
+// v2 wetherforcast  
+app.MapGet("api/v2", () =>
 {
     var now = DateTime.UtcNow;
     return Results.Text(@$"
@@ -29,15 +29,48 @@ app.MapGet("/", () =>
     <link rel='stylesheet' href='https://cdn.simplecss.org/simple-v1.css'>
     </head>
     <body>
-    <h1>Hello from Dotnet on Salus during Demo</h1>
-    <p> Everyone is watching </p>
+    <h1>Hello from Dotnet on Salus during Demo V2 </h1>
+    <p> Weatherforecast Version Two </p>
     <p>The time now in UTC is {now.ToUniversalTime().ToString()} </p>
     </body>
     </html>
     ", "text/html");
 });//.Produces(200, contentType: "text/html");
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("api/v2/weatherforecast/france", () =>
+{
+    var now = DateTime.UtcNow;
+    return Results.Text(@$"
+    <html>
+    <head>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple-v1.css'>
+    </head>
+    <body>
+    <h1>The weather is {Random.Shared.Next(-20, 55)} Degree Celcius in france.  </h1>
+    <p>Version Two. The time now in UTC is {now.ToUniversalTime().ToString()} </p>
+    </body>
+    </html>
+    ", "text/html");
+});//.Produces(200, contentType: "text/html");
+
+    
+app.MapGet("api/v2/weatherforecast/germany", () =>
+{
+    var now = DateTime.UtcNow;
+    return Results.Text(@$"
+    <html>
+    <head>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple-v1.css'>
+    </head>
+    <body>
+      <h1>The weather is {Random.Shared.Next(-20, 55)} Degree Celcius in Germany.</h1>
+      <p>Version Two. The time now in UTC is {now.ToUniversalTime().ToString()} </p>
+    </body>
+    </html>
+    ", "text/html");
+});//.Produces(200, contentType: "text/html");
+   
+app.MapGet("api/v2/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
@@ -50,6 +83,60 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+
+
+//test route
+app.MapGet("test", () =>
+{
+    var now = DateTime.UtcNow;
+    return Results.Text(@$"
+    <html>
+    <head>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple-v1.css'>
+    </head>
+    <body>
+    <h1> Welcome to Salus Test Service </h1>
+    </body>
+    </html>
+    ", "text/html");
+});//.Produces(200, contentType: "text/html");
+
+app.MapGet("test/football-clubs", () =>
+{
+    var now = DateTime.UtcNow;
+    return Results.Text(@$"
+    <html>
+    <head>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple-v1.css'>
+    </head>
+    <body>
+    <h1> Names  </h1>
+    <p> Man City </p>
+    <p> Arsenal <p>
+    <p> Others </p>
+    </body>
+    </html>
+    ", "text/html");
+});//.Produces(200, contentType: "text/html");
+
+    
+app.MapGet("api/v2/weatherforecast/germany", () =>
+{
+    var now = DateTime.UtcNow;
+    return Results.Text(@$"
+    <html>
+    <head>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple-v1.css'>
+    </head>
+    <body>
+      <h1>The weather is {Random.Shared.Next(-20, 55)} Degree Celcius in Germany.</h1>
+      <p>Version Two. The time now in UTC is {now.ToUniversalTime().ToString()} </p>
+    </body>
+    </html>
+    ", "text/html");
+});//.Produces(200, contentType: "text/html");
+
 
 app.Run("http://0.0.0.0:8080");
 
